89a4ca534713a8df627a0456bd7c50981403070e
Lee Sylvester <lee.sylvester@touchtech.co.nz>
2016-11-29 17:27:45 +1300

    Add checks that returned roles form a list

--- lib/model/role.ex
+++ lib/model/role.ex
@@ -18,8 +18,10 @@ defmodule Guardian.Roles.Role do
       def find(%user_mod{} = user, %group_mod{} = grp) do
         user = user |> repo.preload(:roles)
         criteria = %{user_id: user.id, role: 0} |> Map.put_new(group_id, grp.id)
-        Enum.filter(user.roles, fn(r) -> Map.get(r, group_id) == grp.id end)
-          |> List.last || struct(role_mod, criteria)
+        case Enum.filter(user.roles, fn(r) -> Map.get(r, group_id) == grp.id end) do
+          [u|_] -> u
+          [] -> struct(role_mod, criteria)
+        end
       end
 
     end
